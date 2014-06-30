84dabab24edf434069d2ad1399e0c39c314a1cb0
Martin-Andersen <MartinHerlovAndersen@gmail.com>
2014-07-01 00:40:32 +0200

    Adding files

--- app/App.xaml.cs
+++ app/App.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace app
+﻿namespace app
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        public App()
+        {
+            var boot = new Bootstrapper();
+            boot.Run();
+        }
     }
 }
---
+++ app/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.ServiceLocation;
+
+namespace app
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            var shell = ServiceLocator.Current.GetInstance<Shell>();
+            shell.Show();
+            return shell;
+        }
+    }
+}
